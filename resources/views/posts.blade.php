470cafa936250a9eea63e5d27c878e22755ba76e
David Lozano <david.loza010@gmail.com>
2023-09-06 12:48:49 -0400

    feat(adding source code for project): adding source code for project

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a arcu nisi. Etiam dictum convallis viverra. Donec sit amet velit eget lacus varius pharetra ut vitae urna. Donec dapibus at arcu et egestas. Curabitur pulvinar luctus lacus, et mattis massa venenatis quis. Etiam mattis, lectus a finibus pellentesque, quam tortor gravida dui, sit amet dignissim nulla arcu in est. Donec non efficitur lacus, vitae mollis mi. Morbi lorem dui, tempor viverra vulputate sit amet, elementum rhoncus metus. Nullam et augue gravida lectus rutrum pellentesque.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a arcu nisi. Etiam dictum convallis viverra. Donec sit amet velit eget lacus varius pharetra ut vitae urna. Donec dapibus at arcu et egestas. Curabitur pulvinar luctus lacus, et mattis massa venenatis quis. Etiam mattis, lectus a finibus pellentesque, quam tortor gravida dui, sit amet dignissim nulla arcu in est. Donec non efficitur lacus, vitae mollis mi. Morbi lorem dui, tempor viverra vulputate sit amet, elementum rhoncus metus. Nullam et augue gravida lectus rutrum pellentesque.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a arcu nisi. Etiam dictum convallis viverra. Donec sit amet velit eget lacus varius pharetra ut vitae urna. Donec dapibus at arcu et egestas. Curabitur pulvinar luctus lacus, et mattis massa venenatis quis. Etiam mattis, lectus a finibus pellentesque, quam tortor gravida dui, sit amet dignissim nulla arcu in est. Donec non efficitur lacus, vitae mollis mi. Morbi lorem dui, tempor viverra vulputate sit amet, elementum rhoncus metus. Nullam et augue gravida lectus rutrum pellentesque.
+        </p>
+    </article>
+
+</body>
+</html>
